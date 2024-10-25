003f921dd4ebe90f0e0b73bdfc19b6b3930c9e43
Ray <lanskyassassination@gmail.com>
2024-10-25 13:58:57 +0800

    重構 main.cpp，移除未使用的編碼器引腳設置，新增 OLED 初始化及顯示 IR 數值功能

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,18 @@
 #include <SoftwareSerial.h>
 #include "motor_control.h"
 
-// 左右輪的馬達和編碼器引腳
-const int encoderLeftPinA = 2;  // 左輪編碼器 A 相
-const int encoderRightPinA = 3; // 右輪編碼器 A 相
-const int motorLeftPWM = 5;     // 左輪馬達 PWM 控制, 轉速0~255
-const int motorRightPWM = 6;    // 右輪馬達 PWM 控制, 轉速0~255
-const int motorLeftDir = 4;     // 左輪馬達方向
-const int motorRightDir = 7;    // 右輪馬達方向
-const int buttonPin = 8;        // 按鈕連接到 Pin 2
-int buttonState = 0;            // 變量來儲存按鈕狀態
+const int motorLeftPWM = 5;  // 左輪馬達 PWM 控制, 轉速0~255
+const int motorRightPWM = 6; // 右輪馬達 PWM 控制, 轉速0~255
+const int motorLeftDir = 4;  // 左輪馬達方向
+const int motorRightDir = 7; // 右輪馬達方向
+const int buttonPin = 8;     // 按鈕連接到 Pin 2
+int buttonState = 0;         // 變量來儲存按鈕狀態
 
 volatile int IR_LL = 0;
 volatile int IR_L = 0;
 volatile int IR_M = 0;
 volatile int IR_R = 0;
-volatile int IR_RR = 0;       // 最右的紅外線感測器
+volatile int IR_RR = 0;
 volatile long pulseLeft = 0;  // 左輪的脈衝數
 volatile long pulseRight = 0; // 右輪的脈衝數
 
@@ -27,8 +24,6 @@ void setup()
 {
     // 設置馬達和編碼器的引腳
     pinMode(buttonPin, INPUT_PULLUP);
-    pinMode(encoderLeftPinA, INPUT);
-    pinMode(encoderRightPinA, INPUT);
     pinMode(motorLeftPWM, OUTPUT);
     pinMode(motorRightPWM, OUTPUT);
     pinMode(motorLeftDir, OUTPUT);
@@ -39,19 +34,20 @@ void setup()
         pinMode(IR[i], INPUT); // 數值為0~1023，白色為0，黑色為1023
     }
 
-    // 設置編碼器中斷
-    // attachInterrupt(digitalPinToInterrupt(encoderLeftPinA), updateLeftPulse, RISING);
-    // attachInterrupt(digitalPinToInterrupt(encoderRightPinA), updateRightPulse, RISING);
+    OLED_init(); // OLED 初始化
+    // Serial.begin(9600);
 }
 
 void loop()
 {
-    Serial.begin(9600);
+    // 按按鈕
     buttonState = digitalRead(buttonPin);
     while (buttonState)
     {
         buttonState = digitalRead(buttonPin);
+        OLED_display(); // OLED 顯示IR真實數值
     }
+    delay(1000);
 
     while (!(IR_RR)) // 直到RR紅外線看到黑
     {
